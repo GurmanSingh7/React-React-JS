4db81f57e7275ce31dd3cb0afc2986e7477220e9
Gurman Singh <gurmansingh57801@gmail.com>
2026-06-02 21:45:07 +0530

    Update App.jsx

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'    
+
 import './App.css'
+
 import Mnop from './Mnop' // Imported Mnop from Mnop.Jsx from this file 
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <div>
       <h1>Good Morning!</h1>
